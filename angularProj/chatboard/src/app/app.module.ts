2c87840eb5c27ff6b4cd224366a001b4daf928fa
Traian-Pop <traian.v.pop@gmail.com>
2018-03-28 18:11:11 -0500

    fixed bug

--- angularProj/chatboard/src/app/app.module.ts
+++ angularProj/chatboard/src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
-import { SignUpComponent } from './profile/sign-up/sign-up.component';
 import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent,
-    SignUpComponent,
     SignupComponent
   ],
   imports: [
