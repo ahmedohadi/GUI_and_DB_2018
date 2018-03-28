8fe47ce10398ee7e894927aba649779b34e3479d
Traian-Pop <traian.v.pop@gmail.com>
2018-03-28 18:07:39 -0500

    test

--- angularProj/chatboard/src/app/app.module.ts
+++ angularProj/chatboard/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignUpComponent } from './profile/sign-up/sign-up.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    SignUpComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule
