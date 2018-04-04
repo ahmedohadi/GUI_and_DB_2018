f97e39d553e0d2189bdcd6b2ec109930e836c48d
Ahmed Hadi <ahadi@smu.edu>
2018-04-04 17:26:15 -0500

    updated signup page

--- angularProj/chatboard/src/app/app.module.ts
+++ angularProj/chatboard/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { SignupComponent } from './signup/signup.component';
     SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angularProj/chatboard/src/app/signup/signup.component.ts
+++ angularProj/chatboard/src/app/signup/signup.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: []
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  showCandidateList: Boolean;
+  showPartyList: Boolean;
 
-  ngOnInit() {
+  constructor() {
+    this.showCandidateList = false;
+    this.showPartyList = false;
+  }
+  candidateShowHideList() {
+    if (this.showCandidateList === false) {
+      this.showCandidateList = true;
+    } else if (this.showCandidateList === true) {
+      this.showCandidateList = false;
+    }
   }
 
+  partyShowHideList() {
+    if (this.showPartyList === false) {
+      this.showPartyList = true;
+    } else if (this.showPartyList === true) {
+      this.showPartyList = false;
+    }
+  }
+  ngOnInit() {
+  }
 }
